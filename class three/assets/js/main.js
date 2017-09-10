d08fc6d7a2e8f03cacc90a04d130e12661016f60
Sabuj6622 <whimsicalsabuj6622@gmail.com>
2017-09-10 22:54:07 +0600

    class-three

---
+++ class three/assets/js/main.js
@@ -0,0 +1,19 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+     $('.video-paly-btn').magnificPopup({
+       type: 'video',
+     });
+
+    
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+}(jQuery));	

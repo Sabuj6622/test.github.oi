a3fc180267133ae3c71d943e3a5f27e6bfc61aa3
Sabuj6622 <whimsicalsabuj6622@gmail.com>
2017-09-13 19:53:43 +0600

    class-six

---
+++ class-six/assets/js/main.js
@@ -0,0 +1,21 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+$('.image-link').magnificPopup({
+  type: 'image',
+  gallery:{
+    enabled:true
+  }
+});
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	

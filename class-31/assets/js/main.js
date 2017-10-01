d5a17bc8973bfa4c0e7c4a4d2eea64d4b21b9fdf
Sabuj6622 <whimsicalsabuj6622@gmail.com>
2017-10-01 20:17:58 +0600

    class-31

---
+++ class-31/assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+
+  
+        var s = skrollr.init({
+			render: function(data){
+				
+			}
+		});
+
+
+    });
+
+
+    jQuery(window).load(function(){
+      
+        
+    });
+
+
+}(jQuery));	

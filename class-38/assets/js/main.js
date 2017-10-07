bed36c07bc5a1194fec05631f813a8e88ac07063
Sabuj6622 <whimsicalsabuj6622@gmail.com>
2017-10-07 15:14:37 +0600

    class-38

---
+++ class-38/assets/js/main.js
@@ -0,0 +1,31 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+       $(".home-page-slide").owlCarousel({
+		   items:1,
+		   dots:true,
+		   nav:true,
+		   navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+		   autoPlay:false,
+		   loop:true,
+		   animateOut: 'fadeOut',
+	   })
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	

802d084fd064934cbd06d27f1f86d6e18818f825
Sabuj6622 <whimsicalsabuj6622@gmail.com>
2017-10-13 21:11:31 +0600

    class-47

---
+++ class-47/assets/js/main.js
@@ -0,0 +1,18 @@
+(function($){
+	"use strict";
+	jQuery(document).ready(function($)
+	{
+	$(".testimonial-carousel-active").owlCarousel({
+		items:1,
+		loop:true,
+		dots:true,
+		nav:false,
+		autoplay:true,
+	    smartSpeed:3000,
+		});
+		
+		});
+				
+				jQuery(window).load(function(){})})
+				
+	(jQuery);

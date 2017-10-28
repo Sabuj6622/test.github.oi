77921656e4facc4392468de4f28cdf772472d059
Sabuj6622 <whimsicalsabuj6622@gmail.com>
2017-10-28 15:10:34 +0600

    class-60

--- class-60/assets/js/main.js
+++ class-60/assets/js/main.js
@@ -13,11 +13,12 @@
     jQuery(document).ready(function ($) {
 
         
-    var center = [37.772323, -122.214897];
-    $('.map')
+    var center = [22.7671527, 91.5419904];
+    $('#map')
       .gmap3({
         center: center,
         zoom: 13,
+        scrollWheel:false,
         mapTypeId : google.maps.MapTypeId.ROADMAP
       })
       .marker({

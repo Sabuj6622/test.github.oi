ec34fe93d76c92acc0eb3cdc326231f7d8328744
Sabuj6622 <whimsicalsabuj6622@gmail.com>
2017-10-28 14:49:24 +0600

    class-60

--- class-60/assets/js/main.js
+++ class-60/assets/js/main.js
@@ -14,7 +14,11 @@
 
         
 
-        
+        $('#map').gmpa3({
+          address:"Mirsarai,Bangladesh",
+          zoom: 13,
+          mapTypeId : google.maps.MapTypeId.ROADMAP
+        });
         
        
 

b56bb585e837b1dee712bcd1cdc80ce05ceec3ee
Sabuj6622 <whimsicalsabuj6622@gmail.com>
2017-11-19 00:49:31 +0600

    class-84

---
+++ class-84/assets/js/main.js
@@ -0,0 +1,45 @@
+/**
+ *****************************************
+ * main.js
+ *
+ * JS FILE FOR CUSTOM JS AND ACTIVE JS
+ *
+ *****************************************
+ **/
+
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function ($) {
+
+        
+    $('#navigation').slimmenu(
+{
+    resizeWidth: '800',
+    collapserTitle: 'Main Menu',
+    animSpeed: 'medium',
+    easingEffect: null,
+    indentChildren: false,
+    childrenIndenter: '&nbsp;'
+});
+        
+        
+       
+
+    });
+
+
+
+    jQuery(window).load(function () {
+        
+        // -------------------------------------------------------------
+        // animation active
+        // -------------------------------------------------------------
+        new WOW().init();
+        
+        jQuery('#preloader').fadeOut(3000);
+
+    });
+
+
+}(jQuery));

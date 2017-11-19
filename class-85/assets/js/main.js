712430dcf628946c7c15a1a2c557932a7b9fd6e5
Sabuj6622 <whimsicalsabuj6622@gmail.com>
2017-11-19 16:54:04 +0600

    class-85

---
+++ class-85/assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.testimonial-active').owlCarousel({
+    items:1,
+    loop:true,
+    dots:true,
+    nav:true,
+    navText:["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
+    smartSpeed:3000,
+});
+
+});
